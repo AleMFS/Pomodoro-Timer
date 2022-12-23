90270150c62503ac46b0425535eca4e3c149d063
Alexandre Santos <Alexandre_MFS@outlook.com>
2022-12-22 23:54:41 -0300

    Page home

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { CountDownContainer, Separator } from "./styles";
 
 export function Countdown() {
+
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+
+    
     return (
         <CountDownContainer>
             <span>{minutes[0]}</span>
